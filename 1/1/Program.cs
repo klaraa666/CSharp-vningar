fe035f44796b3c5caf30ffaa443f9d4051c02f6d
Klara Hagelin <klara.hagelin@learnet.se>
2022-08-26 14:25:39 +0200

    övningarna

---
+++ 1/1/Program.cs
@@ -0,0 +1,38 @@
+﻿//övning 1
+Console.WriteLine("Hello, World!\n");
+
+//övning 2
+Console.WriteLine("Once upon a time");
+Console.WriteLine("I was falling in love");
+Console.WriteLine("Now I’m only falling apart \n");
+
+//övning3
+Console.WriteLine("Once upon a time \nI was falling in love \nNow I’m only falling apart \n");
+
+//övning4
+string text = "Live and Sleep!\n";
+Console.WriteLine(text);
+
+//övning 5
+string name = "Ada Lovelace";
+Console.WriteLine($"Hej " + name + "! \n");
+
+//övning6
+Console.WriteLine("Give Input!");
+string input = Console.ReadLine();
+Console.WriteLine(input+ "\n");
+
+//övning7
+Console.WriteLine("Give Input!");
+string input2 = Console.ReadLine();
+int i = 1;
+while (i <= 3)
+{
+    Console.WriteLine("\n" + input2);
+    i++;
+}
+
+//övning8
+Console.WriteLine("Hi whats your name?");
+string name2 = Console.ReadLine();
+Console.WriteLine("Hello " + name2 + "!");
